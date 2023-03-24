9a25d3b44d84666140cd164eb4fe570a7eee7687
xXadat_elatXx Creations <123255872+entpawner@users.noreply.github.com>
2023-03-25 01:02:04 +0700

    Update Deer god.lua

--- Deer god.lua
+++ Deer god.lua
@@ -1,77 +1,128 @@
----====== Define spawner ======---
 
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/Source.lua"))()
+local Spawner = loadstring(game:HttpGet('https://raw.githubusercontent.com/MuhXd/DoorSuff/main/OtherSuff/DoorEntitySpawn%2BSource'))()
 
----====== Create entity ======---
 
+-- Create entity
 local entity = Spawner.createEntity({
-    CustomName = "Template Entity",
-    Model = "rbxassetid://12444414843", -- Your entity's model url here ("rbxassetid://1234567890" or GitHub raw url)
-    Speed = 25,
-    MoveDelay = 2,
+    CustomName = "Deer", -- Custom name of your entity
+    Model = "rbxassetid://12444414843", -- Can be GitHub file or rbxassetid
+    Speed = 40, -- Percentage, 100 = default Rush speed
+    DelayTime = 5, -- Time before starting cycles (seconds)
     HeightOffset = 0,
     CanKill = true,
-    KillRange = 50,
-    SpawnInFront = false,
-    ShatterLights = true,
-    FlickerLights = {
-        Enabled = true,
-        Duration = 15
+    NoDieOnCrouching = false,
+    NoHiding = true,
+    AntiCrucifix = false,
+    KillRange = 100,
+    OneRoom = false,
+    DieOnLook = false,
+    BreakLights = true,
+    BackwardsMovement = false,
+     MovementDeath = {
+        true, -- Turned On?
+        '1',  --- '1'= 'Instant Without Being Looked out' | '2' = 'With Being Looked At'
     },
+    FlickerLights = {
+        true, -- Enabled/Disabled
+        20, -- Time (seconds)
+    },
+EntitySounds = { -- Can Get rid Of
+PlaySound = {
+              "https://github.com/MuhXd/Models/blob/main/video0-1-3.mp3?raw=true", -- SoundId Link or Roblox ID
+            { Volume = 0 }, -- Sound properties
+       },
+        
+                   
+          Footsteps = {
+             "https://github.com/MuhXd/Models/blob/main/video0-1-3.mp3?raw=true", -- SoundId Link or Roblox ID
+              { Volume = 0 }, -- Sound properties
+          },
+    }, -- Up to Here
+
     Cycles = {
         Min = 1,
         Max = 1,
-        Delay = 2
+        WaitTime = 2,
     },
     CamShake = {
-        Enabled = true,
-        Values = {1.5, 20, 0.1, 1},
-        Range = 100
+        true, -- Enabled/Disabled
+        {3.5, 20, 0.1, 1}, -- Shake values (don't change if you don't know)
+        100, -- Shake start distance (from Entity to you)
     },
-    ResistCrucifix = false,
-    BreakCrucifix = true,
-    DeathMessage = {"Custom", "death", "message", "goes", "here."},
-    IsCuriousLight = false
+    Jumpscare = {
+        false, -- Enabled/Disabled
+        {
+            Type = "2", -- "Normal" or 1 | "Pop" or 2 | "endlessdoorsrebound" or "Rebound" or 3 | More coming Soon
+            Image1 = "rbxassetid://10483855823", -- Image1 url
+            Image2 = "rbxassetid://10483999903", -- Image2 url
+            Shake = true,
+            Sound1 = {
+                "https://github.com/MuhXd/Models/blob/main/video0-1-3.mp3?raw=true", -- SoundId Link or Roblox ID
+                { Volume = 5 }, -- Sound properties
+            },
+            Sound2 = {
+                "https://github.com/MuhXd/Models/blob/main/video0-1-3.mp3?raw=true", -- SoundId Link or Roblox ID
+                { Volume = 3 }, -- Sound properties
+            },
+            Flashing = {
+                true, -- Enabled/Disabled
+                Color3.fromRGB(255, 255, 255), -- Color
+            },
+            Tease = {
+                true, -- Enabled/Disabled
+                Min = 1,
+                Max = 3,
+            },
+        },
+    },
+    Color = 'CuriousLight', -- CuriousLight ( Yellow ) | GuidingLight ( Blue )
+    DiffrentMessages = true,
+    CustomDialog = {  
+        {"You can", "put your", "custom death", "message here."}, -- Death Messages
+        {"Stop Dieing"},
+        {"Bruh", "Use what you have learned from Rush!"},
+        {"It seems like Template is causing quite the ruckus...", "Hide in a closet or bed as quickly as possible!"},
+         {"I have told You What to do", "YOU JUST HAVE A SKILL ISSUE"}
+    }
 })
 
----====== Debug ======---
+-----[[ Advanced Sctipting ]]-----
 
+entity.Debug.OnEntityMoving = function(Invincible,Hiding,plrCollisionPoint)
+print("Invincible: "..tostring(Invincible))
+print("Player to Entity Collision (None hiding Point): "..tostring(plrCollisionPoint))
+print("Hiding: "..tostring(Hiding))
+end
+       
 entity.Debug.OnEntitySpawned = function()
-    print("Entity has spawned")
+    print("Entity has spawned:")
 end
 
 entity.Debug.OnEntityDespawned = function()
-    print("Entity has despawned")
+    print("Entity has despawned:")
 end
 
 entity.Debug.OnEntityStartMoving = function()
-    print("Entity started moving")
+    print("Entity has started moving:")
 end
 
 entity.Debug.OnEntityFinishedRebound = function()
-    print("Entity finished rebound")
+    print("Entity has finished rebound:")
 end
 
-entity.Debug.OnEntityEnteredRoom = function(room)
-    print("Entity entered room:", room)
+entity.Debug.OnEntityEnteredRoom = function(entityTable, room)
+    print("Entity:", "has entered room:",room)
 end
 
 entity.Debug.OnLookAtEntity = function()
-    print("Player looking at entity")
+    print("Player has looked at entity:")
 end
 
 entity.Debug.OnDeath = function()
-    print("Player has died")
+    warn("Player has died.")
 end
 
---[[
-    NOTE: By overwriting 'OnUseCrucifix', the default crucifixion will be ignored and this function will be called instead
+------------------------
 
-    entity.Debug.OnUseCrucifix = function()
-        print("Custom crucifixion script here")
-    end
-]]--
-
----====== Run entity ======---
-
+-- Run the created entity
 Spawner.runEntity(entity)
